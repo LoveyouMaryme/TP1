f5d2cf4af1d61d2a4f8e6e5b3c87fc47abb58c60
Love <love-mary.victor@outlook.com>
2025-02-01 13:36:45 -0500

    add message de bienvenue

---
+++ FacturationRoulonsVehiculesVerts.java
@@ -0,0 +1,34 @@
+
+/**
+ * Write a description of class FacturationRoulonsVehiculesVerts here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class FacturationRoulonsVehiculesVerts
+{
+    public static void main(String [] args)
+    
+    {
+        // Déclaration de constantes
+        final String MESSAGE_BIENVENUE = "Bienvenue dans le système de facturation de Roulons des véhicules verts (RVV)";
+        final String ENCADRE_TITRE = "---------------------------------------------------------------------------------";
+        final String MESSAGE_MENU_CHOIX = "*** Menu de choix ***";
+        final String CHOIX_UN = "1. Afficher l'inventaire des véhicules";
+        final String CHOIX_DEUX = "2. Facturer la location d’un véhicule";
+        final String CHOIX_TROIS = "3. Afficher le nombre de véhicules hybrides et électriques loués";
+        final String CHOIX_QUATRE = "4. Quitter le programme";
+        
+
+        
+        // Déclaration de variables
+        choix_
+        
+        // Affichage de résultat
+        System.out.println(ENCADRE_TITRE);
+        System.out.println(MESSAGE_BIENVENUE);
+        System.out.println(ENCADRE_TITRE);
+        
+        
+    }
+}

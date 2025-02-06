9214d0bd6089562cf3e367b5d155e9df734b4bcf
Love <love-mary.victor@outlook.com>
2025-02-06 02:01:18 -0500

    feature: added option 1 and 2

--- FacturationRoulonsVehiculesVerts.java
+++ FacturationRoulonsVehiculesVerts.java
@@ -8,7 +8,7 @@
 public class FacturationRoulonsVehiculesVerts
 {
     public static void main(String [] args)
-    
+
     {
         // Déclaration de constantes
         final String NOM_ENTREPRISE = "Roulons les Véhicules Verts (RVV)";
@@ -23,11 +23,7 @@ public class FacturationRoulonsVehiculesVerts
         final String ADRESSE_ENTREPRISE = "1500 rue Matata, Hakuna, Québec Y0Z 6Y7" ;
         final String TELEPHONE_ENTREPRISE = "438 222-1111";
         final String MESSAGE_NOMBRE_VEHICULE_INVENTAIRE = "Nombre de véhicules disponibles dans l'inventaire";
-        
-        
-        
 
-        
         // Déclaration de variables
         byte choixOption;
         int voituresLouesHybridesPetites = 0;
@@ -42,62 +38,63 @@ public class FacturationRoulonsVehiculesVerts
         int voituresRestantesElectriquesIntermediaires = 9;
         int voituresLouesElectriquesGrandes = 0;
         int voituresRestantesElectriquesGrandes = 5;
-        String entreeButton = "";
-        
-        
+        boolean isPressed = false;
+
         // Affichage de résultat
         System.out.println(ENCADRE_TITRE);
         System.out.println(MESSAGE_BIENVENUE);
-        System.out.println(ENCADRE_TITRE + "\n");
-        System.out.printf("%s\n%s\n%s\n%s\n", CHOIX_UN, CHOIX_DEUX, CHOIX_TROIS, CHOIX_QUATRE);
-        System.out.print("\nEntrez votre choix : ");
-        choixOption = Clavier.lireByte();
-        
-            while(choixOption <= 0 || choixOption >= 5) {
-        
-            System.out.println("\nEntrez invalide !\n");
-        
-            System.out.printf("\n%s\n%s\n%s\n%s\n", CHOIX_UN, CHOIX_DEUX, CHOIX_TROIS, CHOIX_QUATRE);
+
+        do {  
+            //Afficher le menu initial
+            System.out.println(ENCADRE_TITRE + "\n");
+            System.out.println(MESSAGE_MENU_CHOIX);
+            System.out.printf("%s\n%s\n%s\n%s\n", CHOIX_UN, CHOIX_DEUX, CHOIX_TROIS, CHOIX_QUATRE);
             System.out.print("\nEntrez votre choix : ");
             choixOption = Clavier.lireByte();
-        
-    
-        
-        
-        }
-    
-    
-        do{
-            
-            System.out.println(ENCADRE_SOUS_TIRE);
-            System.out.println(NOM_ENTREPRISE);
-            System.out.println("Adresse :       " + ADRESSE_ENTREPRISE);
-            System.out.println("Téléphone :     " + TELEPHONE_ENTREPRISE);
-            System.out.println("Date et Heure : " + "26-01-2025 11:25:04");
-            System.out.println(ENCADRE_SOUS_TIRE+ "\n");
-            
-            if (choixOption == 1) {
-                            
-                System.out.println(MESSAGE_NOMBRE_VEHICULE_INVENTAIRE);
-                System.out.println("*************************************************");
-                System.out.println("Grandeur          Hybride      Électrique");
-                System.out.println("****************************************");            
-                System.out.printf("Petit %15d %15d", voituresRestantesHybridesPetites, voituresRestantesElectriquesPetites);
-                System.out.printf("\nIntermédiaire %7d %14d", voituresRestantesHybridesIntermediaires, voituresRestantesElectriquesIntermediaires);
-                System.out.printf("\nGrand %14d %15d\n\n", voituresRestantesHybridesGrandes, voituresRestantesElectriquesGrandes);
-                
-                System.out.print("Appuyer sur <ENTREE> pour réafficher le menu...\n");   
-                entreeButton = Clavier.lireString();
             
             
-                        };
-                      
-            
-         } while ((choixOption > 0 && choixOption <= 4 )|| entreeButton == "\n");
-    
-    
-    
-    
-    };      
-        
-};
+            //Gérer les erreurs d'options  
+            while(choixOption <= 0 || choixOption >= 5) {
+                System.out.println("\nEntrée invalide !\n");
+                System.out.println(MESSAGE_MENU_CHOIX);
+                System.out.printf("\n%s\n%s\n%s\n%s\n", CHOIX_UN, CHOIX_DEUX, CHOIX_TROIS, CHOIX_QUATRE);
+                System.out.print("\nEntrez votre choix : ");
+                choixOption = Clavier.lireByte();
+
+            }
+
+            //Afficher les différents choix
+            switch(choixOption){
+                case 1:
+                    System.out.println(MESSAGE_NOMBRE_VEHICULE_INVENTAIRE);
+                    System.out.println("*************************************************");
+                    System.out.println("Grandeur          Hybride      Électrique");
+                    System.out.println("****************************************");            
+                    System.out.printf("Petit %15d %15d", voituresRestantesHybridesPetites, voituresRestantesElectriquesPetites);
+                    System.out.printf("\nIntermédiaire %7d %14d", voituresRestantesHybridesIntermediaires, voituresRestantesElectriquesIntermediaires);
+                    System.out.printf("\nGrand %14d %15d\n\n", voituresRestantesHybridesGrandes, voituresRestantesElectriquesGrandes);
+                    break;
+                case 2:
+                    System.out.println("Option2");
+                    break;
+                case 3:
+                    System.out.println(MESSAGE_NOMBRE_VEHICULE_INVENTAIRE);
+                    System.out.println("*************************************************");
+                    System.out.println("Grandeur          Hybride      Électrique");
+                    System.out.println("****************************************");            
+                    System.out.printf("Petit %15d %15d", voituresLouesHybridesPetites, voituresLouesElectriquesPetites);
+                    System.out.printf("\nIntermédiaire %7d %14d", voituresLouesHybridesIntermediaires, voituresLouesElectriquesIntermediaires);
+                    System.out.printf("\nGrand %14d %15d\n\n", voituresLouesHybridesGrandes, voituresLouesElectriquesGrandes);
+                    break;
+                case 4:
+                    System.out.println("Option4");
+                    break;
+
+            } 
+            System.out.println("Appuyer sur <ENTREE> pour réafficher le menu...");
+            Clavier.lireFinLigne();
+            isPressed = true;
+
+        }while(isPressed = true);
+    }
+}
